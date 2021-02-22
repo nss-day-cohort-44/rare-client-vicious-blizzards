8e13d26ea963a4c81611578bf28683dd7965d818
Kristen Steele <ksteelephoto@gmail.com>
2021-02-22 15:19:45 -0600

    remove username field in registration form. set username equal to value of email field.

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -3,14 +3,14 @@ import { Link, useHistory } from "react-router-dom"
 // import "./Auth.css"
 
 export const Register = (props) => {
+  const history = useHistory()
+  
   const firstName = useRef()
   const lastName = useRef()
-  const username = useRef()
   const email = useRef()
   const password = useRef()
   const verifyPassword = useRef()
   const passwordDialog = useRef()
-  const history = useHistory()
   const date = new Date()
   const milliDate = date.getTime()
 
@@ -24,7 +24,7 @@ export const Register = (props) => {
         "email": email.current.value,
         "password": password.current.value,
         "bio": "",
-        "username": username.current.value,
+        "username": email.current.value,
         "created_on": milliDate,
         "profile_image_url": "",
         "active": true,
@@ -82,17 +82,6 @@ export const Register = (props) => {
             name="lastName"
             className="form-control"
             placeholder="Last name"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="username"> Username </label>
-          <input
-            ref={username}
-            type="text"
-            name="username"
-            className="form-control"
-            placeholder="username"
             required
           />
         </fieldset>
