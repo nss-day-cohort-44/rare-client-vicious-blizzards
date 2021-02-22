66450ab7ff1dda3f58cf5d33073878149d7d12e4
Charles Butterworth <charlessbutterworth@gmail.com>
2021-02-22 16:08:34 -0600

    categories can be added, deleted. Can also view all categories. End of day commit

--- src/components/categories/CategoryProvider.js
+++ src/components/categories/CategoryProvider.js
@@ -6,16 +6,21 @@ export const CategoryProvider = (props) => {
     const [categories, setCategories] = useState([])
 
     const getCategories = () => {
-        return fetch("http://localhost:8088/categories")
+        return fetch("http://localhost:8000/categories", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
+        })
             .then(res => res.json())
             .then(setCategories)
     }
 
     const addCategory = category => {
-        return fetch("http://localhost:8088/categories", {
+        return fetch("http://localhost:8000/categories", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
             },
             body: JSON.stringify(category)
         })
@@ -23,17 +28,21 @@ export const CategoryProvider = (props) => {
     }
 
     const removeCategory = categoryId => {
-        return fetch(`http://localhost:8088/categories/${categoryId}`, {
-            method: "DELETE"
+        return fetch(`http://localhost:8000/categories/${categoryId}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
         })
             .then(getCategories)
     }
 
     const updateCategory = category => {
-        return fetch(`http://localhost:8088/categories/${category.id}`, {
+        return fetch(`http://localhost:8000/categories/${category.id}`, {
             method: "PUT",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
             },
             body: JSON.stringify(category)
         })
